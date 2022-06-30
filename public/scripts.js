2fd67216aabb8e30b435f7006211ee55ea156789
Abhishek Paul <abhishek.paul0705@gmail.com>
2022-06-30 15:29:51 -0400

    yes

--- public/scripts.js
+++ public/scripts.js
@@ -1,6 +1,6 @@
 async function getStat(endpoint){
     try{
-        const response = await fetch('http://localhost:3002/'+endpoint)
+        const response = await fetch('https://spotfystats1.herokuapp.com/'+endpoint)
         data = await response;
         return await data.json()
     }
